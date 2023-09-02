20814e67ab362483211d0de64270243cadeda8e3
Mahboob-Alam <iammahboob.a@gmail.com>
2023-09-02 14:47:45 +0530

    updated as per 2nd review

--- musicproject/settings.py
+++ musicproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ieid&zz^!10-@rw1vdo!@*wnccb367dw@v@ema%i8_)b9%6fvj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ravipython.pythonanywhere.com']
 
 
 # Application definition
