2121f54cf9dc8b4e01de9051a969bfe8717a09e1
Mahboob Alam <iammahboob.a@gmail.com>
2023-09-04 11:29:34 +0530

    Search music (#7)
    * added search by song, album and artist name
    
    * added distinct in search functionality
    
    * updated as per the first review
    
    * updated as per 2nd review
    
    * updated as per 2nd review
    
    * final update of search functionality

--- music/views.py
+++ music/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.db.models import Q
+
 from .models import Song
 
 def index(request):
-    allSongs = Song.objects.all()
-    return render(request, template_name="index.html", context={"allSongs" : allSongs})
+    allSongs = Song.objects.all().order_by('-last_updated')
+    return render(request, template_name="music/index.html", context={"allSongs" : allSongs})
+
+
+def search_songs(request): 
+    template_path = 'music/search_result.html'
+    
+    search_query = request.GET.get('search', None)
+
+    if search_query: 
+        search_result = Song.objects.filter(
+            Q(songName__icontains=search_query) | 
+            Q(album__albumName__icontains=search_query) | 
+            Q(album__artist__artistName__icontains=search_query)
+        ).distinct()
+    else: 
+        search_result = Song.objects.all()
+        
+    context = {'search_result' : search_result, 'search_query' : search_query}
+    return render(request, template_path, context)
